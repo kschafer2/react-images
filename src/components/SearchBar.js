2275f27995f27d186dd90d68a7e595f2b0a08dac
Kyle Schafer <kschay@protonmail.com>
2019-08-21 13:43:01 -0500

    Closes #3 - Refactored text input to controlled element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  // event handler called when and only when text input changes
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
 
   render() {
     return (
@@ -12,7 +9,11 @@ class SearchBar extends React.Component {
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
