8b4cd7b0850a5eb1589f6f61774fc2bdb8a00ee9
Kyle Schafer <kschay@protonmail.com>
2019-08-21 13:27:54 -0500

    Closes #2 - Implemented onInputChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  // event handler called when and only when text input changes
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input type="text" onChange={this.onInputChange} />
           </div>
         </form>
       </div>
