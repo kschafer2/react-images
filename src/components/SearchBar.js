2deb6844f0a43e771e117527ec1cef079d95bec4
Kyle Schafer <kschay@protonmail.com>
2019-08-21 14:16:38 -0500

    Closes #5 - Passed search term to App

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,12 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
-  //arrow function makes sure 'this' references this object
+  // arrow function makes sure 'this' references this object
   onFormSubmit = (event) => {
     event.preventDefault();
 
-    console.log(this.state.term);
-  }
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
