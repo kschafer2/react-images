a1e56c1e8f6f09e05a08a72a0eb28cea68cb8967
Kyle Schafer <kschay@protonmail.com>
2019-08-21 15:44:38 -0500

    Closes #6 - Added Unsplash image GET request with 'term' parameter

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+         'Client-ID eed5dbcc90e4b13e9ad086521c4c39d8a166595906ce6a3ab7bfc7782fa8d1dd'
+      }
+    });
   }
 
   render() {
